69d16f358e5f25d7ec9263a9504a829970b10849
David <david-1.fischer@novartis.com>
2020-04-11 21:47:25 +0200

    First commit

---
+++ lego.R
@@ -0,0 +1,22 @@
+library(dplyr)
+sets = read.csv('sets.csv')
+inventories = read.csv('inventories.csv')
+inventory_parts = read.csv('inventory_parts.csv')
+
+mysets = c(60243,
+           31034,
+           60222,
+           60214,
+           60110,
+           60212,
+           75890,
+           31075,
+           60243,
+           60181,
+           30359,
+           10706)
+
+for (i in 1:length(mysets)){
+  
+}
+
